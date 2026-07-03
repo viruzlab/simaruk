e695b273fffefbf4c49f83c758df7e2b03675187
gilangnug2990 <gilangnug2990@gmail.com>
2026-07-03 16:23:32 +0700

    Menambahkan favicon logo1.png

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>SIMARUK - Sistem Peminjaman Ruangan Akademik</title>
+    <link rel="icon" href="{{ asset('img/logo1.png') }}" type="image/png">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
